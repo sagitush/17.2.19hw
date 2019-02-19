117d17214bf2693450d77dfd82de04c9839b9f01
sagitush <44786027+sagitush@users.noreply.github.com>
2019-02-19 20:09:49 +0200

    Add files via upload

---
+++ 17.2.19h.w/17.2.19h.w/Flash.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._2._19h.w
+{
+    class Flash:Human,IFlash
+    {
+        public int Voltage { get;private set; }
+
+        public Flash(int voltage,int age,string name):base(name,age)
+        {
+            Voltage = voltage;
+        }
+       public void FireLightnings()
+        {
+            Console.WriteLine("fire,fire");
+        }
+        public override string Name
+        {
+            get
+            {
+                return this.Name;
+            }
+            set
+            {
+                if (Name != null)
+                {
+                    this.Name = Name;
+
+                }
+
+            }
+
+        }       
+    }
+}
---
+++ 17.2.19h.w/17.2.19h.w/Human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._2._19h.w
+{
+    interface ISuperHero
+    {
+        void ActiveSuperPowers();
+    }
+    interface IFlash:ISuperHero
+    {
+        void FireLightnings();
+    }
+    interface IClimb:ISuperHero
+    {
+        void Climb();
+    }
+    interface IFly:ISuperHero
+    {
+        void Fly();
+    }
+    abstract class Human
+    {
+        public abstract string Name { get; set; }
+        private int age;
+
+        public int Age { get;  }
+        public Human(string name,int age)
+        {
+            this.age = age;
+            this.Name = name;
+        }
+    }
+    
+}
